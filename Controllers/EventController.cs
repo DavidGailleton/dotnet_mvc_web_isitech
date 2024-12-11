bbe3a0837ebbaa42192be8a15d1a5f5e2284ce30
David GAILLETON <david.gailleton@posteo.net>
2024-12-11 16:12:08 +0100

    Add: CRUD EventController.cs

---
+++ Controllers/EventController.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Mvc;
+using MVC_cours_isitech.data;
+using MVC_cours_isitech.Models;
+
+namespace MVC_cours_isitech.Controllers;
+
+public class EventController : Controller
+{
+    private readonly ApplicationDbContext _context;
+    
+    public EventController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        var events = _context.Events.ToList();
+        return View(events);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Event @event)
+    {
+        _context.Events.Add(@event);
+        
+        _context.SaveChanges();
+        
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        var @event = _context.Events.Find(id);
+        
+        return View(@event);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Event @event)
+    {
+        _context.Events.Update(@event);
+        
+        _context.SaveChanges();
+        
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public IActionResult Delete(int id)
+    {
+        var @event = _context.Events.Find(id);
+        
+        _context.Events.Remove(@event);
+        _context.SaveChanges();
+        
+        return RedirectToAction("Index");
+    }
+}
